ca447133265cf2ffccffaaa4bbc753a02e6c21d5
Tim Groß <44930867+Timhot3p@users.noreply.github.com>
2022-04-25 00:34:14 +0200

    Just some empty lines

--- GuildPet/Classes.cs
+++ GuildPet/Classes.cs
@@ -13,14 +13,23 @@ namespace GuildPet
             switch (cls)
             {
                 case 1: return 5;
+
                 case 2: return 2;
+
                 case 3: return 4;
+
                 case 4: return 4;
+
                 case 5: return 5;
+
                 case 6: return 4;
+
                 case 7: return 4;
+
                 case 8: return 4;
+
                 case 9: return 5;
+
                 default: return 0;
             }
         }
@@ -30,14 +39,23 @@ namespace GuildPet
             switch (cls)
             {
                 case 1: return 50;
+
                 case 2: return 10;
+
                 case 3: return 25;
+
                 case 4: return 25;
+
                 case 5: return 10;
+
                 case 6: return 50;
+
                 case 7: return 50;
+
                 case 8: return 25;
+
                 case 9: return 50;
+
                 default: return 0;
             }
         }
@@ -99,14 +117,23 @@ namespace GuildPet
             switch (cls)
             {
                 case 1: return 1;
+
                 case 2: return 3;
+
                 case 3: return 2;
+
                 case 4: return 2;
+
                 case 5: return 1;
+
                 case 6: return 1;
+
                 case 7: return 2;
+
                 case 8: return 3;
+
                 case 9: return 3;
+
                 default: return 0;
             }
         }
@@ -116,14 +143,23 @@ namespace GuildPet
             switch (cls)
             {
                 case 1: return 0.25f;
+
                 case 2: return 0;
+
                 case 3: return 0.5f;
+
                 case 4: return 0.5f;
+
                 case 5: return 0;
+
                 case 6: return 0.5f;
+
                 case 7: return 0;
+
                 case 8: return 0.5f;
+
                 case 9: return 0.25f;
+
                 default: return 0.0f;
             }
         }
